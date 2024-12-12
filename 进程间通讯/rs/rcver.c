455737ab1168a0b4b646fa0e66d197aec5deb0e5
Iiguoren <1329314711@qq.com>
2024-12-12 23:45:09 +0800

    1

--- 进程间通讯/rs/rcver.c
+++ 进程间通讯/rs/rcver.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
 #include <proto.h>
 int main(){
     key_t key;
--- 进程间通讯/rs/sdner.c
+++ 进程间通讯/rs/sdner.c
@@ -1,9 +1,14 @@
 #include <proto.h>
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
 int main(){
     key_t key;
     int msgid;
     key = ftok(KEYPATH, KEYPROJ);
+    msgid = msgget(key, IPC_CREAT|0600);
     if(key<0){
         perror("ftok()");
         exit(1);
